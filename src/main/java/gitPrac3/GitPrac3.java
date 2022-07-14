e39324a7ea7ca73c7788ba88c9e530af1e12ffe4
shrutisaxenaa <106474924+shrutisaxenaa@users.noreply.github.com>
2022-07-14 17:35:32 -0500

    second commit

--- src/main/java/gitPrac3/GitPrac3.java
+++ src/main/java/gitPrac3/GitPrac3.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class GitPrac3 {
@@ -19,6 +20,13 @@ public class GitPrac3 {
 		driver.manage().deleteAllCookies();
 		driver.get("https://techfios.com/billing/?ng=admin/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		
+		driver.close();
+		driver.quit();
 	}
 
 }
