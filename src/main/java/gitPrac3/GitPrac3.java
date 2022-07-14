1ada6da79b312c647f7279b68a56208ed1980a76
shrutisaxenaa <106474924+shrutisaxenaa@users.noreply.github.com>
2022-07-14 17:11:42 -0500

    first commit

---
+++ src/main/java/gitPrac3/GitPrac3.java
@@ -0,0 +1,24 @@
+package gitPrac3;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GitPrac3 {
+	
+	WebDriver driver;
+	
+	@Test
+	public void init() {
+		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://techfios.com/billing/?ng=admin/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+}
